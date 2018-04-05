3124f28ddbb3118647dc0acdfc780aaf13ea0ec0
Moises Nuñez <manc1221@gmail.com>
2018-04-05 13:29:54 -0500

    Updated contentful show and edit system

--- app/controllers/contentful_controller.rb
+++ app/controllers/contentful_controller.rb
@@ -2,26 +2,25 @@ class ContentfulController < ApplicationController
   skip_before_action :authenticate_user!
   include ApplicationHelper
 
-  def show
+  def index
     #@entry = entry
   end
 
+  def show
+    @entry = contentful.entry(params[:id])
+  end
+
   def edit
-    @entry = entry
+    @entry = contentful.entry(params[:id])
   end
 
   def update
-
+    entry = contentful_management.entries.find(params[:id])
     entry.update(title: params[:Title], description: params[:Description])
     entry.publish
 
-    flash[:notice] = "successfully updated"
+    flash[:notice] = "successfully updated, this can take a few minutes"
 
-    redirect_to contentful_show_path
-  end
-  private
-  def entry
-    content_types = contentful_management.content_types.find('course')
-    entry = contentful_management.entries.find('1toEOumnkEksWakieoeC6M')
+    redirect_to contentful_index_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   post 'contentful/update', to: 'contentful#update'
-  get 'contentful/show', to: 'contentful#show'
+  get 'contentful/index', to: 'contentful#index'
   get 'contentful/edit', to: 'contentful#edit'
+  get 'contentful/show', to: 'contentful#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
